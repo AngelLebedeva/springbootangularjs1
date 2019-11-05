4e3abef840ef83ee9be9332c3bf8936dab7e5af4
Anzhelika Lebedeva <fikfa_@mail.ru>
2019-11-05 15:32:01 +0700

    Initial commit

---
+++ src/main/java/com/startspring/springbootangularjs1/SpringbootangularjsApplication.java
@@ -0,0 +1,16 @@
+package com.startspring.springbootangularjs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+    @SpringBootApplication
+    public class SpringbootangularjsApplication {
+
+
+        public static void main(String[] args) {
+
+            SpringApplication.run(SpringbootangularjsApplication.class, args);
+        }
+    }
+
+
---
+++ src/main/java/com/startspring/springbootangularjs1/controller/MainRESTController.java
@@ -0,0 +1,78 @@
+package com.startspring.springbootangularjs1.controller;
+
+import com.startspring.springbootangularjs1.dao.EmployeeDAO;
+import com.startspring.springbootangularjs1.model.Employee;
+import com.startspring.springbootangularjs1.model.EmployeeForm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MainRESTController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+
+    @RequestMapping(value = "/employees", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public List<Employee> getEmployees() {
+        List<Employee> list = employeeDAO.getAllEmployees();
+        return list;
+    }
+
+    @RequestMapping(value = "/employee/{empId}", //
+            method = RequestMethod.GET, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee getEmployee(@PathVariable("empId") Long empId) {
+        return employeeDAO.getEmployee(empId);
+    }
+
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.POST, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee addEmployee(@RequestBody EmployeeForm empForm) {
+
+        System.out.println("(Service Side) Creating employee with empNo: " + empForm.getEmpNo());
+
+        return employeeDAO.addEmployee(empForm);
+    }
+
+    @RequestMapping(value = "/employee", //
+            method = RequestMethod.PUT, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, //
+                    MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public Employee updateEmployee(@RequestBody EmployeeForm empForm) {
+
+        System.out.println("(Service Side) Editing employee with Id: " + empForm.getEmpId());
+
+        return employeeDAO.updateEmployee(empForm);
+    }
+
+    @RequestMapping(value = "/employee/{empId}", //
+            method = RequestMethod.DELETE, //
+            produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+    @ResponseBody
+    public void deleteEmployee(@PathVariable("empId") Long empId) {
+
+        System.out.println("(Service Side) Deleting employee with Id: " + empId);
+
+        employeeDAO.deleteEmployee(empId);
+    }
+
+}
